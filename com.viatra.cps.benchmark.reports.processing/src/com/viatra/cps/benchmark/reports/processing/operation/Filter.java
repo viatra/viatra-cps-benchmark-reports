cd11a918c6252c49c83bed7a3f476acb70b99d37
zole40 <zole40@gmail.com>
2018-03-01 11:32:55 +0100

    created filter Operation

---
+++ com.viatra.cps.benchmark.reports.processing/src/com/viatra/cps/benchmark/reports/processing/operation/Filter.java
@@ -0,0 +1,24 @@
+package com.viatra.cps.benchmark.reports.processing.operation;
+
+import java.util.List;
+
+public class Filter extends Operation {
+
+	public Filter(String attribute, List<Object> filter) {
+		super(attribute, filter);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void calculate() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
