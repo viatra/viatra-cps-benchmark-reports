f337b3d5e3e511541d426beedde7cb6402c51423
zole40 <zole40@gmail.com>
2018-03-01 12:48:30 +0100

    updated diagram, deleted OperationType

--- com.viatra.cps.benchmark.reports.processing/src/com/viatra/cps/benchmark/reports/processing/operation/OperationFactory.java
+++ com.viatra.cps.benchmark.reports.processing/src/com/viatra/cps/benchmark/reports/processing/operation/OperationFactory.java
@@ -3,13 +3,13 @@ package com.viatra.cps.benchmark.reports.processing.operation;
 import java.util.List;
 
 public class OperationFactory {
-	public static Operation createOperation(OperationType operationType,List<Object> filter,String attribute) {
+	public static Operation createOperation(String operationType,List<Object> filter,String attribute) {
 		switch (operationType) {
-		case Average:
+		case "Average":
 			return new Average(attribute, filter);
-		case Mean:
+		case "Mean":
 			return new Mean(attribute, filter);
-		case Summary:
+		case "Summary":
 			return new Summary(attribute, filter);
 		default:
 			return null;
--- com.viatra.cps.benchmark.reports.processing/src/com/viatra/cps/benchmark/reports/processing/operation/OperationType.java
+++
@@ -1,7 +0,0 @@
-package com.viatra.cps.benchmark.reports.processing.operation;
-
-public enum OperationType {
-	Average,
-	Mean,
-	Summary
-}
