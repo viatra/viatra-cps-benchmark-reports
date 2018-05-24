38aa1b5522063fcca3bf1fad024d820137de78c7
Béres Zoltán <zole40@gmail.com>
2018-05-24 14:53:55 +0200

    added jar location

--- com.viatra.cps.benchmark.reports.processing/run.sh
+++ com.viatra.cps.benchmark.reports.processing/run.sh
@@ -7,6 +7,7 @@ resultVisualizerLocation=$4
 buildConfigTemplate=$5
 diagramConfigTemplate=$6
 updateConfig=$7
+jarLocation=$8
 echo "Results location: "$resultsLocation
 echo "Case: "$case
 echo "aggregatorConfig:"$aggregatorConfig
@@ -17,5 +18,5 @@ echo "Update Digram Config: " $updateConfig
 for build in "${builds[@]}"
 do
   echo "$build"
-  java -jar ./com.viatra.cps.benchmark.reports.processing-0.0.1-jar-with-dependencies.jar  -r "${resultsLocation}/$case/$build/results.json" -c "${aggregatorConfig}" -a "${resultVisualizerLocation}/resultVisualizer/src/results/" -bt "${buildConfigTemplate}" -dt "${diagramConfigTemplate}" -bs "${resultVisualizerLocation}/resultVisualizer/src/config/builds.json" -dc "${resultVisualizerLocation}/resultVisualizer/src/config/diagram.config.json" -b "$case-$build" -u ${updateConfig}
+  java -jar "{$jarLocation}"/com.viatra.cps.benchmark.reports.processing-0.0.1-jar-with-dependencies.jar  -r "${resultsLocation}/$case/$build/results.json" -c "${aggregatorConfig}" -a "${resultVisualizerLocation}/resultVisualizer/src/results/" -bt "${buildConfigTemplate}" -dt "${diagramConfigTemplate}" -bs "${resultVisualizerLocation}/resultVisualizer/src/config/builds.json" -dc "${resultVisualizerLocation}/resultVisualizer/src/config/diagram.config.json" -b "$case-$build" -u ${updateConfig}
 done
