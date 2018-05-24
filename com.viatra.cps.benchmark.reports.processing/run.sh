69d531fa28b3c42f208d654fd1ffb412827313cd
Béres Zoltán <zole40@gmail.com>
2018-05-24 10:24:36 +0200

    added all expected input parameter to run.sh

--- com.viatra.cps.benchmark.reports.processing/run.sh
+++ com.viatra.cps.benchmark.reports.processing/run.sh
@@ -1,8 +1,18 @@
-read -a arr
-tool=$1
-echo $tool
-for i in "${arr[@]}"
+read -a builds
+resultsLocation=$1
+case=$2
+aggregatorConfig=$3
+resultVisualizerLocation=$4
+buildConfigTemplate=$5
+diagramConfigTemplate=$6
+echo $resultsLocation
+echo $case
+echo $aggregatorConfig
+echo $resultVisualizerLocation
+echo $buildConfigTemplate
+echo $diagramConfigTemplate
+for build in "${builds[@]}"
 do
-  echo "$i"
-  java -jar ./com.viatra.cps.benchmark.reports.processing-0.0.1-jar-with-dependencies.jar  -r "/mnt/g/viatra-cps-benchmark-results-master/$tool/$i/results.json" -c "aggregatorConfig.json" -a "./../resultVisualizer/src/results/" -bt "./build.config.template.json" -dt "./diagram.config.template.json" -bs "./../resultVisualizer/src/config/builds.json" -dc "./../resultVisualizer/src/config/diagram.config.json" -b "$tool-$i"
+  echo "$build"
+  java -jar ./com.viatra.cps.benchmark.reports.processing-0.0.1-jar-with-dependencies.jar  -r "${resultsLocation}/$case/$build/results.json" -c "${aggregatorConfig}" -a "${resultVisualizerLocation}/resultVisualizer/src/results/" -bt "${buildConfigTemplate}" -dt "${diagramConfigTemplate}" -bs "${resultVisualizerLocation}/resultVisualizer/src/config/builds.json" -dc "${resultVisualizerLocation}/resultVisualizer/src/config/diagram.config.json" -b "$case-$build"
 done
