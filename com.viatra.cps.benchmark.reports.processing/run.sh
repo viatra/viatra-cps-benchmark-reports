879b99055546bda6eb1f31d9365e4820f03e6e24
Béres Zoltán <zole40@gmail.com>
2018-08-02 17:06:55 +0200

    #19 cleared run

--- com.viatra.cps.benchmark.reports.processing/run.sh
+++ com.viatra.cps.benchmark.reports.processing/run.sh
@@ -17,7 +17,7 @@ echo "Jar location: " $jarLocation
 for build in "${builds[@]}"
 do
   echo "$build"
-  java -jar "$jarLocation/com.viatra.cps.benchmark.reports.processing-0.0.1-jar-with-dependencies.jar"  -b "${BuildID}" -i "${resultsLocation=$1}" -c "${aggregatorConfig}" -o "${outputResults}" -p "${processingConfig}" -d "${diagramConfigTemplate}" -v "${visualizerConfiguration}"
+  java -jar "$jarLocation/com.viatra.cps.benchmark.reports.processing-0.0.1-jar-with-dependencies.jar"  -b "${BuildID}" -i "${inputResults}" -o "${outputResults}" -p "${processingConfig}" -d "${diagramConfigTemplate}" -v "${visualizerConfiguration}"
 done
 
 
