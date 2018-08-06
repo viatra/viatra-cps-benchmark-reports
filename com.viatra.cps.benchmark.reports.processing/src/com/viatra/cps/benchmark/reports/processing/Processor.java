e02dfeb22fd47ba98e557a5cc0efe570b2a4bdd8
Béres Zoltán <zole40@gmail.com>
2018-08-06 11:11:47 +0200

    Revert "#19 check input is json"
    This reverts commit d84381a5c3ce51ae5a5d9e775404ed0f3b6ed3b0.

--- com.viatra.cps.benchmark.reports.processing/src/com/viatra/cps/benchmark/reports/processing/Processor.java
+++ com.viatra.cps.benchmark.reports.processing/src/com/viatra/cps/benchmark/reports/processing/Processor.java
@@ -57,7 +57,7 @@ public class Processor {
 		this.buildId = buildId;
 		// Initialize objectmapper
 		mapper = new ObjectMapper();
-
+		
 		mapper.configure(SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS, false);
 		mapper.configure(SerializationConfig.Feature.INDENT_OUTPUT, true);
 		// turn off autodetection
@@ -74,8 +74,7 @@ public class Processor {
 		this.configuration = this.loadConfiguration(new File(configPath));
 
 		// Load or create visualizer configuration
-		this.visualizerConfiguration = this
-				.loadVisualizerConfiguration(new File(visualizerConfigPath + "/config.json"));
+		this.visualizerConfiguration = this.loadVisualizerConfiguration(new File(visualizerConfigPath + "/config.json"));
 
 		// Load or create Builds.json
 		this.builds = this.loadBuilds(new File(resultOutputPath + "/builds.json"));
@@ -213,30 +212,26 @@ public class Processor {
 		try (Stream<Path> paths = Files.walk(this.resultInputPath)) {
 			paths.filter(Files::isRegularFile).forEach((path) -> {
 				try {
-					if (this.getFileExtension(path.toFile()) != "json") {
-						System.out.println(path.toFile().getName() +"is not a json");
-					} else {
-						BenchmarkResult result = mapper.readValue(path.toFile(), BenchmarkResult.class);
-						this.updateVisualizerConfig(result);
-						Map<String, List<BenchmarkResult>> scenarioMap = caseScenarioMap
-								.get(result.getCaseDescriptor().getCaseName());
-						if (scenarioMap != null) {
-							List<BenchmarkResult> benchmarkList = (List<BenchmarkResult>) scenarioMap
-									.get(result.getCaseDescriptor().getScenario());
-							if (benchmarkList != null) {
-								benchmarkList.add(result);
-							} else {
-								benchmarkList = new ArrayList<>();
-								benchmarkList.add(result);
-								scenarioMap.put(result.getCaseDescriptor().getScenario(), benchmarkList);
-							}
+					BenchmarkResult result = mapper.readValue(path.toFile(), BenchmarkResult.class);
+					this.updateVisualizerConfig(result);
+					Map<String, List<BenchmarkResult>> scenarioMap = caseScenarioMap
+							.get(result.getCaseDescriptor().getCaseName());
+					if (scenarioMap != null) {
+						List<BenchmarkResult> benchmarkList = (List<BenchmarkResult>) scenarioMap
+								.get(result.getCaseDescriptor().getScenario());
+						if (benchmarkList != null) {
+							benchmarkList.add(result);
 						} else {
-							List<BenchmarkResult> resultList = new ArrayList<>();
-							resultList.add(result);
-							scenarioMap = new HashMap<>();
-							scenarioMap.put(result.getCaseDescriptor().getScenario(), resultList);
-							caseScenarioMap.put(result.getCaseDescriptor().getCaseName(), scenarioMap);
+							benchmarkList = new ArrayList<>();
+							benchmarkList.add(result);
+							scenarioMap.put(result.getCaseDescriptor().getScenario(), benchmarkList);
 						}
+					} else {
+						List<BenchmarkResult> resultList = new ArrayList<>();
+						resultList.add(result);
+						scenarioMap = new HashMap<>();
+						scenarioMap.put(result.getCaseDescriptor().getScenario(), resultList);
+						caseScenarioMap.put(result.getCaseDescriptor().getCaseName(), scenarioMap);
 					}
 				} catch (IOException e) {
 					e.printStackTrace();
@@ -272,22 +267,6 @@ public class Processor {
 		}
 	}
 
-	private String getFileExtension(File file) {
-		String extension = "";
-
-		try {
-			if (file != null && file.exists()) {
-				String name = file.getName();
-				extension = name.substring(name.lastIndexOf("."));
-			}
-		} catch (Exception e) {
-			extension = "";
-		}
-
-		return extension;
-
-	}
-
 	public void updateVisualizerConfig(BenchmarkResult result) {
 		result.getPhaseResults().forEach(phaseResult -> phaseResult.getMetrics().forEach(metric -> {
 			if (!this.visualizerConfiguration.getScales().stream()
@@ -315,17 +294,17 @@ public class Processor {
 
 		counter = this.configuration.size();
 		lock = new Object();
-		File resultJson = Paths.get(this.resutOutputPath.toString(), this.buildId, caseName, scenario, "results.json")
-				.toFile();
-		File diagramJson = Paths
-				.get(this.resutOutputPath.toString(), this.buildId, caseName, scenario, "diagram.config.json").toFile();
+		File resultJson = Paths.get(this.resutOutputPath.toString(), this.buildId, caseName, scenario, "results.json").toFile();
+		File diagramJson = Paths.get(this.resutOutputPath.toString(), this.buildId, caseName, scenario, "diagram.config.json").toFile();
 		mapper.writeValue(diagramJson, new Diagrams(this.buildId + "/" + caseName + "/" + scenario));
 		mapper.writeValue(resultJson, new Results(this.buildId + "/" + caseName + "/" + scenario));
 		this.configuration.forEach(aggConfig -> {
 			Operation last = null;
-			JSonSerializer tmp = new JSonSerializer(resultJson, diagramJson, aggConfig.getID(),
-					this.buildId + "/" + caseName + "/" + scenario, this.diagramConfiguration, caseName, scenario,
-					this.diagramSet, this.dashboardConfigurationFile, this.buildId, this.mapper);
+			JSonSerializer tmp = new JSonSerializer(
+					resultJson,
+					diagramJson,
+					aggConfig.getID(), this.buildId + "/" + caseName + "/" + scenario, this.diagramConfiguration,
+					caseName, scenario, this.diagramSet, this.dashboardConfigurationFile, this.buildId,this.mapper);
 			tmp.setProcessor(this);
 			List<OperationConfig> opconf = aggConfig.getOperations(false);
 			for (OperationConfig opconfig : opconf) {
@@ -373,9 +352,8 @@ public class Processor {
 			}
 			if (build != null) {
 				Case caseElement = null;
-				Optional<Case> optional = build.getCases().stream().filter(c -> c.getCaseName().equals(caseName))
-						.findFirst();
-				if (optional.isPresent()) {
+				Optional<Case> optional = build.getCases().stream().filter(c -> c.getCaseName().equals(caseName)).findFirst();
+				if(optional.isPresent()) {
 					caseElement = optional.get();
 				}
 				if (caseElement != null) {
