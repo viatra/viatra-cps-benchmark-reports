798910c5d90ad61087e98eae4c210c06377a7205
Béres Zoltán <zole40@gmail.com>
2018-08-22 14:00:25 +0200

    updated clone-results

--- clone-results.sh
+++ clone-results.sh
@@ -3,7 +3,6 @@
 if [ -d "viatra-cps-benchmark-results" ]; then
   # Repo exists, update
   rm -rf viatra-cps-benchmark-results
-else
+fi
   # Clone repo
   git clone git@github.com:viatra/viatra-cps-benchmark-results.git viatra-cps-benchmark-results
-fi
